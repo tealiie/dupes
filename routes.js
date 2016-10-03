6dec4fd69f9b0fdf4b0c78e701d26f6656d7d10b
tealiie <tealiie@live.com>
2016-10-03 16:23:58 +1300

    server index running

--- routes.js
+++ routes.js
@@ -3,17 +3,17 @@ var development = require('./knexfile').development
 var knex = require('knex')(development)
 
 module.exports = {
-  category: category,
-  getBrands: getBrands,
-  getProducts: getProducts,
-  getDupes: getDupes
+  cate: cate
+  // brands: brands,
+  // getProducts: getProducts,
+  // getDupes: getDupes
 }
 
 //shows in the initial index of cate
 function cate (req, res) {
   knex('cate')
   .select()
-  .then(function ('cate') {
+  .then(function (cate) {
     res.render('index', { cate: cate})
   })
   .catch(function (err) {
@@ -22,31 +22,27 @@ function cate (req, res) {
 }
 
 //to get the brands of a category
-function getBrands (req, res) {
-  knex('cate')
-  .join('brands', 'cate.id', '=', 'products.cate_id')
-  .select('cate.name' 'brands.name as category')
-  .then(function (data) {
-    res.render(data)
-  })
-}
+// function getBrands (req, res) {
+//   knex('brands')
+//   .join('cate')
+// }
 
 //to get the products of a certain brand
-function getProducts (req, res) {
-  knex('brands')
-  .join('products', 'brands.name', '=', 'products.brand_id')
-  .select('brands.name', 'products.name as products')
-  .then(function (data) {
-    res.render(data)
-  })
-}
-
-//to get the dupes of a product
-  function getDupes (req, res) {
-  knex('products')
-  .join('dupes', 'products.id', '=', 'dupes.product_id')
-  .select('products.colour', 'dupes.colour as dupes')
-  .then(function (data) {
-    res.render(data)
-  })
-}
+// function getProducts (req, res) {
+//   knex('brands')
+//   .join('products', 'brands.name', '=', 'products.brand_id')
+//   .select('brands.name', 'products.name as products')
+//   .then(function (data) {
+//     res.render(data)
+//   })
+// }
+//
+// //to get the dupes of a product
+//   function getDupes (req, res) {
+//   knex('products')
+//   .join('dupes', 'products.id', '=', 'dupes.product_id')
+//   .select('products.colour', 'dupes.colour as dupes')
+//   .then(function (data) {
+//     res.render(data)
+//   })
+// }
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var path = require('path')
 var express = require('express')
 var hbs = require ('express-handlebars')
-var routes = ('./routes/index')
 var bodyParser = require('body-parser')
 
+var index = require('./routes')
+
 var PORT = 3000
+
 var app = express()
+app.engine('hbs', hbs())
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.engine('hbs', hbs())
-app.set('views engine', 'hbs')
-app.set('views', path.join(__dirname + 'views'))
-// app.use(express.static('public'))
-
-app.get('/cate')
+app.get('/', index.cate)
 
 app.listen(PORT, function () {
   console.log('Listening on port', PORT)
