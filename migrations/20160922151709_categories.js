b0fe86dddfe309ed48e9c93419dc5dfd7826f4e3
tealiie <tealiie@live.com>
2016-10-05 20:31:01 +1300

    index products brands done

--- migrations/20160922151709_categories.js
+++ migrations/20160922151709_categories.js
@@ -2,6 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('categories', function (table) {
     table.increments('id').primary()
     table.string('category')
+    table.string('image')
   })
 };
 
--- seeds/categories.js
+++ seeds/categories.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
   return knex('categories').del()
     .then(function () {
       return Promise.all([
-        knex('categories').insert({id: 1, category: 'Lipstick'}),
-        knex('categories').insert({id: 2, category: 'Foundation'}),
-        knex('categories').insert({id: 3, category: 'Eye Shadow'})
+        knex('categories').insert({id: 1, category: 'Lipstick', image: '/categories/lipstick.png'}),
+        knex('categories').insert({id: 2, category: 'Foundation', image: '/categories/foundation.png'}),
+        knex('categories').insert({id: 3, category: 'Eye Shadow', image: '/categories/eyeshadow.png'})
       ]);
     });
 };
